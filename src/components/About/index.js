94d3f68bdd030bf4c2b0410c37110b83e60d7a20
Muneeb-F <m5farruk@uwaterloo.ca>
2023-01-18 23:31:45 -0500

    Made Site Responsive and updated About Desc

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -31,7 +31,7 @@ const About = () => {
                     />
                 </h1>
                 <p>
-                    Welcome to my portfolio! I'm Muneeb and I am a Software Engineering student at the University of Waterloo. I began software development 3 years ago and have a wealth of experience and knowledge to offer. I am always looking for new challenges and opportunities to grow, and am constantly seeking to improve my skills and knowledge.
+                    Hey! I'm Muneeb and I am a Software Engineering student at the University of Waterloo. I began software development 3 years ago and have a wealth of experience and knowledge to offer. I am always looking for new challenges and opportunities to grow, and am constantly seeking to improve my skills and knowledge.
                 </p>
                 <p>
                     I am a team player and have excellent communication skills, which have helped me to build strong relationships with my colleagues and clients. I am also highly organized and able to work well under pressure, which has helped me to deliver projects on time and to a high standard.
